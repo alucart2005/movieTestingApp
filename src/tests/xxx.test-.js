8858323a0f6584a3fed75a3bfa87a71bbb7f898a
Napoleon Anaya Romero <113813382+alucart2005@users.noreply.github.com>
2023-03-19 13:47:36 -0500

    end

--- src/tests/xxx.test-.js
+++
@@ -1,9 +0,0 @@
-const { expectCt } = require("helmet");
-
-const multiply = (a,b) => {
-    return a * b;
-}
-
-test("multiply 2*2 should return 4", () => {
-  expect(multiply(2,2)).toBe(4); // 2 * 2 = 4
-});
